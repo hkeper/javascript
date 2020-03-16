f444516a2a1bfaebe50090ab9c16d7675830f67d
hkeper <hkeper1@gmail.com>
2020-03-16 12:38:14 +0300

    Add fies

---
+++ tasks/calc.js
@@ -0,0 +1,14 @@
+let comboA = 6.75;
+let comboB = 7.5;
+let comboC = 5.75;
+let comboD = 8;
+let comboE = 8.25;
+
+let sum = comboA + comboB + comboC + comboD;
+sum = sum + comboE;
+
+let numberOfCombo = 5;
+
+let avg = sum/numberOfCombo;
+
+avg;
